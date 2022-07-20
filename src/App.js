751a4e8cf06db9baa6dfaaaa59fb030207cd38cc
Mateus <creztfallen@M.S.M>
2022-07-20 14:26:01 -0300

    weekly react lessons done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import Expenses from './components/Expenses';
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
+export default function App() {
+
   const expenses = [
     {
       id: 'e1',
       title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
+      amount: "$500.12",
+      date: new Date(2022, 6, 19),
     },
     {
       id: 'e2',
       title: 'New TV',
       amount: 799.49,
-      date: new Date(2021, 2, 12),
+      date: new Date(2022, 6, 12),
     },
     {
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
-      date: new Date(2021, 2, 28),
+      date: new Date(2022, 6, 28),
     },
     {
       id: 'e4',
       title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
+      amount: 450.33,
+      date: new Date(2022, 5, 12),
     },
   ];
 
@@ -35,6 +36,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
-
